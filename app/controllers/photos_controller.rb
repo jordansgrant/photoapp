a9429de23c50fbf5969537c566e4dc9b43fcda57
jordansgrant <jordans.grant@gmail.com>
2017-09-01 09:09:49 -0600

    Implemented Photo Upload
    * Added Photo Uploader, carrierwave uploader that creates a 100x100 thumbnail and places both in /public/photos
    * Added Photo Model, linked to uploader
    * Added Photo Controller with new, create, and show actions
    * Created resources routes for photos, only new, show, create
    * Created new and show views for photos

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,27 @@
+class PhotosController < ApplicationController
+
+  def new
+    @photo = Photo.new
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      flash[:success] = "Your photo was uploaded!"
+      redirect_to photo_url(@photo)
+    else
+      flash[:error] = "We hit a snag, photo not uploaded"
+      render 'new'
+    end
+  end
+ 
+  private
+    def photo_params
+      params.require(:photo).permit(:photo)
+    end
+ 
+end
